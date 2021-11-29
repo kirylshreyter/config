3fd6f8841385eb512805c0addb612e1942793bfe
Yury Matusevich <yury.a.matusevich@gmail.com>
2021-11-29 21:03:51 +0100

    [UNTY-289] fix connecting to database when fetching configs from DB

--- lib/config/sources/db_source.rb
+++ lib/config/sources/db_source.rb
@@ -9,24 +9,32 @@ module Config
       def initialize(table)
         @table = table.to_s
         @db_config = File.read(Config.database_yml_path)
+        @retry_count = 0
       end
 
-      # returns a config hash from the YML file
       def load
-        config = YAML.load(db_config)[Rails.env]
-        connection_pool = ActiveRecord::Base.establish_connection(config)
+        connection_pool = ActiveRecord::Base.connection_pool
+        connection_pool.lock_thread = true
+        conn = ActiveRecord::Base.retrieve_connection
 
-        if table && connection_pool.connection && connection_pool.connected? && connection_pool.connection.table_exists?(table)
+        if table && conn && connection_pool.connected? && conn.table_exists?(table)
           sql = "select `#{Config.key_key}`, `#{Config.value_key}` from #{table};"
           file_contents = { table => parse_values(ActiveRecord::Base.connection.execute(sql).to_h) }
           result = file_contents.with_indifferent_access
         end
 
-        connection_pool.connection.close if connection_pool.connected?
+        connection_pool.connection.close if connection_pool.active_connection?
+        connection_pool.disconnect! if connection_pool.connected?
 
-        result || {}
+        result.presence || {}
       rescue ActiveRecord::NoDatabaseError
         {}
+      rescue ActiveRecord::ConnectionNotEstablished => exception
+        config = YAML.load(db_config)[Rails.env]
+        ActiveRecord::Base.establish_connection(config)
+        @retry_count += 1 and retry if @retry_count < 1
+
+        raise exception
       end
 
       def parse_values(file_contents)
